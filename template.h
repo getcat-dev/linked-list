e98378bdea4db707ab744291a28ea98c2321e5ca
Margarita <babich222222222@gmail.com>
2021-02-25 17:43:50 +0200

    add header

--- template.h
+++ template.h
@@ -1,24 +1,25 @@
 #ifndef LINKED_LIST_TEMPLATE_H
 #define LINKED_LIST_TEMPLATE_H
+struct University
+{
+    string profession;
+    int gradeYear;
+    bool usabilityOfDiploma;
+};
 
 struct Node
 {
-    struct University
-    {
-        string profession;
-        int gradeYear;
-        bool usabilityOfDiploma;
-    };
+    University diploma;
     Node* pNext;
 };
 struct LinkedList {
-    void push_front(){}
-    void push_back(){}
-    void get(){}
-    void pop_front(){}
-    void pop_back(){}
+    void push_front(Node *header , Node *newNode){}
+    void push_back(Node *tail, Node *newNode){}
+    void get(int n){}
+    void pop_front(Node *header){}
+    void pop_back(Node *tail){}
     void size(){}
-    void print(){}
+    void print(Node *header){}
 private:
     Node *head;
     Node *tail;
